2ddacc19f32b5a8a0a06d992065633536b2b006b
Mohd Shahid <shahid@gmail.com>
2025-01-22 00:31:24 +0530

    import new components

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,24 @@
 import { WavyBackgroundDemo } from "@/components/weavyBackground";
-import { FeaturesSectionDemo } from "@/components/featureBentoGrid";
 import { TabsDemo } from "@/components/tabs";
 import { AccordionDemo } from "@/components/accordian-faq";
+import { Featurecards } from "@/components/featurecards";
+import InfinitySlider from "@/components/infinite-slider";
 import Image from "next/image";
+import ApiFeature from "@/components/apifeature";
 
 export default function Home() {
   return (
     <>
-
       <WavyBackgroundDemo/>
-      <FeaturesSectionDemo/>
+      <InfinitySlider/>
+      <Featurecards/>
       <div className="bg-gray-50 p-16"> 
       <TabsDemo/>
       </div>
+      <ApiFeature/>
       <div className="flex h-[70vh] justify-center max-w-7xl mx-auto px-4 py-20">
         <div className="md:w-1/2 mt-24 flex flex-col gap-2 ">
         <h2 className="text-6xl">Frequently Asked Questions</h2>
-
         </div>
         <div className="md:w-1/2 h-full flex justify-center"><AccordionDemo/></div>
       </div>
